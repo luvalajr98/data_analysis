b6abffcfac1327d6ac58cfd1c3c8faad80e412ba
luvalajr98 <luvalajrcalvin98@gmail.com>
2024-08-02 23:36:48 +0300

    updated the tabnine

---
+++ weatherData.py
@@ -0,0 +1,10 @@
+import sys
+
+# Define a character
+char = 'a'
+
+# Get the size of the character
+char_size = sys.getsizeof(char)
+
+# Print the size of the character
+print(f"The size of the character '{char}' is: {char_size} bytes")
